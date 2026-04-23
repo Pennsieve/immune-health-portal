b7150690c3e8410b18efc51d71a845fe3ec91885
Eric Gauzens <ericgauzens@hotmail.com>
2026-04-23 11:56:08 -0400

    updated contentful import

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -4,7 +4,9 @@
  * Provides access to Contentful client for fetching CMS content
  * like hero sections, team bios, service descriptions, etc.
  */
-import { createClient, type ContentfulClientApi, type EntrySkeletonType } from 'contentful'
+import contentful, { type ContentfulClientApi, type EntrySkeletonType } from 'contentful'
+
+const { createClient } = contentful
 
 export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig()
