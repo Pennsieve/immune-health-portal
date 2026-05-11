0f1b53defc74d93f2114f3cd7f928595fedb1853
Eric Gauzens <ericgauzens@hotmail.com>
2026-05-11 14:23:39 -0400

    updated email

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     // Private keys (server-side only)
     mailersendApiKey: process.env.MAILERSEND_API_TOKEN || '',
-    mailersendFromEmail: process.env.MAILERSEND_FROM_EMAIL || 'test-q3enl6k3n8042vwr.mlsender.net',
+    mailersendFromEmail: process.env.MAILERSEND_FROM_EMAIL || 'noreply@test-q3enl6k3n8042vwr.mlsender.net',
     mailersendFromName: process.env.MAILERSEND_FROM_NAME || 'Immune Health Portal',
     adminEmail: process.env.ADMIN_EMAIL || 'immune.health.admin@gmail.com',
 
